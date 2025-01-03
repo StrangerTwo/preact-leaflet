e7decd410cd08da3d113d93be8f3894eb3e1412d
StrangerTwo <h.volhejn@gmail.com>
2025-01-03 21:28:03 +0100

    export MapContext

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
 export { MapContainer } from './components/MapContainer';
 export { Marker } from './components/Marker';
 export { TileLayer } from './components/TileLayer';
+
+export { MapContext } from './contexts/MapContext';
